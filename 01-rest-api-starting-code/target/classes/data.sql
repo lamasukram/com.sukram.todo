d1f16cf078749893216350d993fa747bb2d912cd
sukram lama <lamasukram56@gmail.com>
2025-08-18 19:32:47 +0900

    backend

---
+++ 01-rest-api-starting-code/target/classes/data.sql
@@ -0,0 +1,6 @@
+insert into todo(id,description,done,target_date,username)
+values(1001,'Learn Aws',false,CURRENT_DATE(),'sukram');
+insert into todo(id,description,done,target_date,username)
+values(1002,'Learn springBoot',false,CURRENT_DATE(),'sukram');
+insert into todo(id,description,done,target_date,username)
+values(1003,'java',false,CURRENT_DATE(),'sukram');
